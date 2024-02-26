3d297b4a98294aa56f6eb64f077a3963cbee46c6
k1cker666 <pomoymu@gmail.com>
2024-02-26 18:09:52 +0300

    feat game: added enemy sprites

---
+++ src/engine/entity/enemy.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.sprite import _Group
+
+class Enemy(pygame.sprite.Sprite):
+    
+    def __init__(self,):
+        super().__init__
--- src/engine/sprites.py
+++ src/engine/sprites.py
@@ -5,6 +5,11 @@ def load_image(image_name):
     result.set_colorkey((255, 255, 255))
     return result 
 
+def load_image_black_bg(image_name):
+    result = pygame.image.load(image_name)
+    result.set_colorkey((0, 0, 0))
+    return result 
+
 class Sprites:
     background = pygame.image.load('./images/testbg.png')
     block_cell = pygame.image.load('./images/sprites/BlockCell.png')
@@ -36,3 +41,71 @@ class Sprites:
     move_left = [player_left_1, player_left_2, player_left_3, player_left_2, player_left_1]
     move_down = [player_down_1, player_down_2, player_down_3, player_down_2, player_down_1]
     move_up = [player_up_1, player_up_2, player_up_3, player_up_2, player_up_1]
+    
+    blinky_right_1 = load_image_black_bg('./images/sprites/Enemy/Blinky/Right_1.png')
+    blinky_right_2 = load_image_black_bg('./images/sprites/Enemy/Blinky/Right_2.png')
+    
+    blinky_left_1 = load_image_black_bg('./images/sprites/Enemy/Blinky/Left_1.png')
+    blinky_left_2 = load_image_black_bg('./images/sprites/Enemy/Blinky/Left_2.png')
+    
+    blinky_down_1 = load_image_black_bg('./images/sprites/Enemy/Blinky/Down_1.png')
+    blinky_down_2 = load_image_black_bg('./images/sprites/Enemy/Blinky/Down_2.png')
+    
+    blinky_up_1 = load_image_black_bg('./images/sprites/Enemy/Blinky/Up_1.png')
+    blinky_up_2 = load_image_black_bg('./images/sprites/Enemy/Blinky/Up_2.png')
+    
+    blinky_move_right = [blinky_right_1, blinky_right_2]
+    blinky_move_left = [blinky_left_1, blinky_left_2]
+    blinky_move_down = [blinky_down_1, blinky_down_1]
+    blinky_move_up = [blinky_up_1, blinky_up_2]
+    
+    clyde_right_1 = load_image_black_bg('./images/sprites/Enemy/Clyde/Right_1.png')
+    clyde_right_2 = load_image_black_bg('./images/sprites/Enemy/Clyde/Right_2.png')
+    
+    clyde_left_1 = load_image_black_bg('./images/sprites/Enemy/Clyde/Left_1.png')
+    clyde_left_2 = load_image_black_bg('./images/sprites/Enemy/Clyde/Left_2.png')
+    
+    clyde_down_1 = load_image_black_bg('./images/sprites/Enemy/Clyde/Down_1.png')
+    clyde_down_2 = load_image_black_bg('./images/sprites/Enemy/Clyde/Down_2.png')
+    
+    clyde_up_1 = load_image_black_bg('./images/sprites/Enemy/Clyde/Up_1.png')
+    clyde_up_2 = load_image_black_bg('./images/sprites/Enemy/Clyde/Up_2.png')
+    
+    clyde_move_right = [clyde_right_1, clyde_right_2]
+    clyde_move_left = [clyde_left_1, clyde_left_2]
+    clyde_move_down = [clyde_down_1, clyde_down_1]
+    clyde_move_up = [clyde_up_1, clyde_up_2]
+    
+    inky_right_1 = load_image_black_bg('./images/sprites/Enemy/Inky/Right_1.png')
+    inky_right_2 = load_image_black_bg('./images/sprites/Enemy/Inky/Right_2.png')
+    
+    inky_left_1 = load_image_black_bg('./images/sprites/Enemy/Inky/Left_1.png')
+    inky_left_2 = load_image_black_bg('./images/sprites/Enemy/Inky/Left_2.png')
+    
+    inky_down_1 = load_image_black_bg('./images/sprites/Enemy/Inky/Down_1.png')
+    inky_down_2 = load_image_black_bg('./images/sprites/Enemy/Inky/Down_2.png')
+    
+    inky_up_1 = load_image_black_bg('./images/sprites/Enemy/Inky/Up_1.png')
+    inky_up_2 = load_image_black_bg('./images/sprites/Enemy/Inky/Up_2.png')
+    
+    inky_move_right = [inky_right_1, inky_right_2]
+    inky_move_left = [inky_left_1, inky_left_2]
+    inky_move_down = [inky_down_1, inky_down_1]
+    inky_move_up = [inky_up_1, inky_up_2]
+    
+    pinky_right_1 = load_image_black_bg('./images/sprites/Enemy/Pinky/Right_1.png')
+    pinky_right_2 = load_image_black_bg('./images/sprites/Enemy/Pinky/Right_2.png')
+    
+    pinky_left_1 = load_image_black_bg('./images/sprites/Enemy/Pinky/Left_1.png')
+    pinky_left_2 = load_image_black_bg('./images/sprites/Enemy/Pinky/Left_2.png')
+    
+    pinky_down_1 = load_image_black_bg('./images/sprites/Enemy/Pinky/Down_1.png')
+    pinky_down_2 = load_image_black_bg('./images/sprites/Enemy/Pinky/Down_2.png')
+    
+    pinky_up_1 = load_image_black_bg('./images/sprites/Enemy/Pinky/Up_1.png')
+    pinky_up_2 = load_image_black_bg('./images/sprites/Enemy/Pinky/Up_2.png')
+    
+    pinky_move_right = [pinky_right_1, pinky_right_2]
+    pinky_move_left = [pinky_left_1, pinky_left_2]
+    pinky_move_down = [pinky_down_1, pinky_down_1]
+    pinky_move_up = [pinky_up_1, pinky_up_2]
